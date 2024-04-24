0426af60553162d0f961b980ac491c0348d4bbd9
Toto <korotkovegor84@gmail.com>
2024-04-24 19:32:27 +0300

    Refac of controller logic (classes added)

--- helpers.php
+++ helpers.php
@@ -43,3 +43,27 @@ function basePath($path = '') {
         echo "Partial {$name} not found!";
     }
  }
+
+ /**
+  * Inspect a value
+  * @param mixed $value
+  * @return void
+  */
+
+  function inspect($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+  }
+
+  /**
+  * Inspect a value and die
+  * @param mixed $value
+  * @return void
+  */
+
+  function inspectAndDie($value){
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+  }
